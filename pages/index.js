44b4b349ef3f1447dafda0b4d333c446803d3dc5
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-06 13:06:28 -0500

    Add card component & styling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/banner";
+import Card from "../components/card";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -23,6 +24,20 @@ export default function Home() {
         />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={1200} height={450} />
+        </div>
+        <div className={styles.cardLayout}>
+          <Card
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            href="/coffee-stores/darkhourse-coffee"
+            className={styles.card}
+          />
+          <Card
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            href="/coffee-stores/darkhourse-coffee"
+            className={styles.card}
+          />
         </div>
       </main>
     </div>
