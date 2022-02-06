ff6e7da7eaacd6617d1d3835b48ab40003c4f8c6
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-05 19:31:01 -0500

    Add Dynamic routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+const CoffeeStore = () => {
+  const router = useRouter();
+  console.log("router", router);
+  return (
+    <div>
+      Coffee Store Page{router.query.id}
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <a>Go to page dynamic</a>
+      </Link>
+    </div>
+  );
+};
+
+export default CoffeeStore;
