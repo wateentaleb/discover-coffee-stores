aa21d008a856b9de0d3a0e29902e5f8e2e618dbc
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-07 10:49:05 -0500

    Leverage serveless functions

--- pages/index.js
+++ pages/index.js
@@ -29,15 +29,18 @@ export default function Home(props) {
   useEffect(async () => {
     if (latLong) {
       try {
-        const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
-        console.log({ fetchedCoffeeStores });
-        // setCoffeeStores(fetchedCoffeeStores);
+        // const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+        const res = await fetch(
+          `/api/getCoffeeStoresByLocation?latLong=${latLong}&limit=30`
+        );
+        const coffeeStores = await res.json();
         dispatch({
           type: ACTION_TYPES.SET_COFFEE_STORES,
           payload: {
-            coffeeStores: fetchedCoffeeStores,
+            coffeeStores,
           },
         });
+        setCoffeeStoresError("");
       } catch (error) {
         setCoffeeStoresError(error.message);
       }
